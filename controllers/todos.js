e68dc29e7e4fc7cf607d75bc7b9a43983379b68c
Kristelle Tasane <kristelle.tasane@voco.ee>
2024-11-12 11:02:15 +0200

    Update the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,12 +1,12 @@
 import { Todo } from "../models/todo.js";
 
 class todoController {
-    constructor(){
+    constructor() {
         // hold todo objects in array
         this.TODOS = []
     }
 
-    createTodo(req, res){
+    createTodo(req, res) {
         // get data from Post request
         const task = req.body.task
         // create new object via Todo model
@@ -21,8 +21,33 @@ class todoController {
         })
     }
 
-    getTodos(req, res){
-        res.json({tasks: this.TODOS})
+    getTodos(req, res) {
+        res.json({ tasks: this.TODOS })
+    }
+
+    updateTodo(req, res) {
+        // get id form url params
+        const todoId = req.params.id
+        // get the updated task name form request body (like form data)
+        const updatedTask = req.body.task
+        // get the array element index if todo id is equal with url params id
+        const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+        // if url params id is not correct - sends error message
+        if (todoIndex < 0) {
+            throw new Error('Could not find todo!')
+            res.json({
+                message: 'Could not find todo with such index'
+            })
+        }
+        // if id is ok - updtae Todo
+        // for update create element with the same id and new task
+        // and save it in the same array element by this index
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+        // show updated info
+        res.json({
+            message: 'Updated todo',
+            updatedTask: this.TODOS[todoIndex]
+        })
     }
 }
 
